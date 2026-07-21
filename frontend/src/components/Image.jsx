a2ff499808f2c9cf82e5381af73e9dc38eadbaf1
Perry Huang <perryhuang2015@gmail.com>
2026-07-21 16:53:18 -0400

    feat: add menu on click

--- frontend/src/components/Image.jsx
+++ frontend/src/components/Image.jsx
@@ -1,9 +1,14 @@
 import waldo from "../assets/waldo.jpg";
 import "./Image.css";
+import { useState } from "react";
 
 export default function Image() {
+    const [xPos, setXPos] = useState(100);
+    const [yPos, setYPos] = useState(100);
     const onClick = (e) => {
         const domImgRect = e.target.getBoundingClientRect();
+        setXPos(e.clientX);
+        setYPos(e.clientY);
         // console.log(domImgRect);
         // console.log("Mouse x location: " + e.clientX);
         // console.log("Mouse y location: " + e.clientY);
@@ -14,5 +19,21 @@ export default function Image() {
         console.log("Relative mouse x location in decimal: " + relX);
         console.log("Relative mouse y location in decimal: " + relY);
     };
-    return <img src={waldo} onClick={onClick} alt="a where's waldo puzzle" />;
+    return (
+        <div>
+            <img src={waldo} onClick={onClick} alt="a where's waldo puzzle" />
+            <div
+                style={{
+                    position: "absolute",
+                    height: "100px",
+                    width: "200px",
+                    top: yPos + "px",
+                    left: xPos + "px",
+                    background: "#FFFFFF",
+                }}
+            >
+                "hello"
+            </div>
+        </div>
+    );
 }
