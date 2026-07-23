5d97698f7e4777790095cf4d478aa1b92b51c4eb
Perry Huang <perryhuang2015@gmail.com>
2026-07-23 19:50:31 -0400

    feat: set up prisma and initial tables

---
+++ backend/src/script.js
@@ -0,0 +1,23 @@
+import { prisma } from "./lib/prisma.js";
+
+async function main() {
+    // Create a new user with a post
+    const picture = await prisma.picture.create({
+        data: {},
+    });
+    console.log("Created picture:", picture);
+
+    // Fetch all users with their posts
+    const allPics = await prisma.picture.findMany();
+    console.log("All pics:", JSON.stringify(allPics, null, 2));
+}
+
+main()
+    .then(async () => {
+        await prisma.$disconnect();
+    })
+    .catch(async (e) => {
+        console.error(e);
+        await prisma.$disconnect();
+        process.exit(1);
+    });
