5ddab5bae3f177a6a6dc525d1dead32996fcf467
Perry Huang <perryhuang2015@gmail.com>
2026-07-28 10:46:59 -0400

    feat: add eslint and prettier

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,11 +1,22 @@
 import express from "express";
+import "dotenv/config";
 
 const app = express();
-const PORT_NUM = 3000;
+
+// parses json requests
+app.use(express.json());
+
+// parses post requests
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
     res.send("hello");
 });
 
-console.log("listening at 127.0.0.1:" + PORT_NUM);
-app.listen(PORT_NUM);
+const PORT = process.env.PORT;
+app.listen(PORT, (err) => {
+    if (err) {
+        throw err;
+    }
+    console.log(`App listening at http://127.0.0.1:${PORT}`);
+});
