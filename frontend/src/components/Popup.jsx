ea9729216e186dd26447502850477399b89c9ab6
Perry Huang <perryhuang2015@gmail.com>
2026-08-26 16:56:57 -0400

    feat: add popup component

---
+++ frontend/src/components/Popup.jsx
@@ -0,0 +1,45 @@
+export default function Popup({ imgNum, xPos, yPos, relX, relY, characters }) {
+    const handleCheck = async (e, charNum) => {
+        const check = await fetch(
+            import.meta.env.VITE_BACKEND_URL +
+                "pictures/" +
+                imgNum +
+                "/validation/" +
+                charNum,
+            {
+                method: "POST",
+                body: {
+                    x: relX,
+                    y: relY,
+                },
+            },
+        );
+        console.log(check);
+    };
+
+    // each button has a character's name and will fetch from backend to
+    // check for that character when the button is pressed
+    const checkButtons = [];
+    for (const char of characters) {
+        checkButtons.push(
+            <button onClick={(e) => handleCheck(e, char.charNum)}>
+                {char.name}
+            </button>,
+        );
+    }
+
+    return (
+        <div
+            style={{
+                position: "absolute",
+                height: "100px",
+                width: "200px",
+                top: yPos + "px",
+                left: xPos + "px",
+                background: "#FFFFFF",
+            }}
+        >
+            {checkButtons}
+        </div>
+    );
+}
