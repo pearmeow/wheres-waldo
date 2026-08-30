9b38af5a493a7bc97f08eae865b5ba9ac9517597
Perry Huang <perryhuang2015@gmail.com>
2026-08-30 18:39:27 -0400

    feat: add better messages to errors

--- backend/src/middleware/validator.js
+++ backend/src/middleware/validator.js
@@ -1,10 +1,25 @@
 import { param, query, body } from "express-validator";
 
 export const createIdParamCheck = (idName) =>
-    param(idName).notEmpty().isInt({ min: 1 }).escape();
+    param(idName)
+        .notEmpty()
+        .withMessage(idName + " cannot be empty")
+        .isInt({ min: 1 })
+        .withMessage("Must be an integer greater than 0")
+        .escape();
 
 export const createIdQueryCheck = (idName) =>
-    query(idName).notEmpty().isInt({ min: 1 }).escape();
+    query(idName)
+        .notEmpty()
+        .withMessage(idName + " cannot be empty")
+        .isInt({ min: 1 })
+        .withMessage("Must be an integer greater than 0")
+        .escape();
 
 export const createFloatBodyCheck = (fieldName) =>
-    body(fieldName).notEmpty().isFloat().escape();
+    body(fieldName)
+        .notEmpty()
+        .withMessage(fieldName + " cannot be empty")
+        .isFloat()
+        .withMessage("Must be a float")
+        .escape();
