0defe5b8fc0d1ebdfc693e8465c6033245524f08
Perry Huang <perryhuang2015@gmail.com>
2026-08-30 18:39:47 -0400

    refactor: merge validation route with pictures

--- backend/src/routes/validation.js
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import * as validation from "../controllers/validation.js";
-// import controller for pictures here
-
-const router = Router();
-
-router.post("/:characterId", validation.post);
-
-export default router;
