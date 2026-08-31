0d027c87d3ad36750c1274809436debd48759f15
Perry Huang <perryhuang2015@gmail.com>
2026-08-30 21:53:55 -0400

    test: add a lot more tests for validation

--- backend/src/tests/validation.test.js
+++ backend/src/tests/validation.test.js
@@ -10,26 +10,72 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use("/pictures", routes.pictures);
 
-describe("validation routes", () => {
-    test("returns true with correct coordinates", async () => {
-        const res = await request(app)
-            .post("/pictures/1/validation/1")
-            .send({ x: 0.69, y: 0.36 });
-        expect(res.body.errors).toBeUndefined();
-        expect(res.body.correct).toBeTruthy();
+describe("validation tests", () => {
+    describe("coordinate tests", () => {
+        test("returns true with correct coordinates", async () => {
+            const res = await request(app)
+                .post("/pictures/1/validation/1")
+                .send({ x: 0.69, y: 0.36 });
+            expect(res.body.errors).toBeUndefined();
+            expect(res.body.correct).toBeTruthy();
+        });
+        test("returns false on incorrect coordinates", async () => {
+            const res = await request(app)
+                .post("/pictures/1/validation/1")
+                .send({ x: 0.66, y: 0.26 });
+            expect(res.body.errors).toBeUndefined();
+            expect(res.body.correct).toBeFalsy();
+        });
+        test("returns errors without coordinates", async () => {
+            const res = await request(app)
+                .post("/pictures/1/validation/1")
+                .send({ abc: "abc" });
+            expect(res.body.errors).toBeDefined();
+            expect(res.body.correct).toBeUndefined();
+        });
     });
-    test("returns false on incorrect coordinates", async () => {
-        const res = await request(app)
-            .post("/pictures/1/validation/1")
-            .send({ x: 0.66, y: 0.26 });
-        expect(res.body.errors).toBeUndefined();
-        expect(res.body.correct).toBeFalsy();
-    });
-    test("returns errors without coordinates", async () => {
-        const res = await request(app)
-            .post("/pictures/1/validation/1")
-            .send({ abc: "abc" });
-        expect(res.body.errors).toBeDefined();
-        expect(res.body.correct).toBeUndefined();
+    describe("picture and character id tests", () => {
+        test("returns 404 with string picture id", (done) => {
+            request(app)
+                .post("/pictures/adkfjas/validation/1")
+                .send({ abc: "abc" })
+                .expect(404, done);
+        });
+        test("returns 404 with out of bounds picture id", (done) => {
+            request(app)
+                .post("/pictures/9999/validation/1")
+                .send({ abc: "abc" })
+                .expect(404, done);
+        });
+        test("returns 404 with negative picture id", (done) => {
+            request(app)
+                .post("/pictures/-1/validation/1")
+                .send({ abc: "abc" })
+                .expect(404, done);
+        });
+        test("returns 404 with string character id", (done) => {
+            request(app)
+                .post("/pictures/1/validation/aksdjf")
+                .send({ abc: "abc" })
+                .expect(404, done);
+        });
+        test("returns 404 with out of bounds character id", (done) => {
+            request(app)
+                .post("/pictures/1/validation/9999")
+                .send({ abc: "abc" })
+                .expect(404, done);
+        });
+        test("returns 404 with negative character id", (done) => {
+            request(app)
+                .post("/pictures/1/validation/-1")
+                .send({ abc: "abc" })
+                .expect(404, done);
+        });
+        test("returns 404 with character id that does not belong to the picture", (done) => {
+            request(app)
+                .post("/pictures/3/validation/1")
+                .send({ abc: "abc" })
+                .expect(404, done);
+        });
     });
 });
